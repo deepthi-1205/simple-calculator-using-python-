7260c6a3f79eb9c9ab3f0f329b40b329851cfbf6
deepthi-1205 <kumardeepthi079@gmail.com>
2026-02-23 22:33:29 +0530

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,27 @@
+while True:
+    a=int(input("enter the number"))
+    b=int(input("enter the number"))
+    c=input("enter the operation ( + - * % / ^):")
+
+    if(c=="+"):
+        print(a+b)
+    if(c=="-"):
+        print(a-b)
+    if(c=="*"):
+        print(a*b)
+    if(c=="%"):
+        print(a%b)
+    if(c=="/"):
+        if(b==0):
+            print("invalid operation ")
+            continue
+        print(a/b)
+    if(c=="^"):
+        print(a**b)
+    d=input("if you want to continue:(yes/no)")
+    if(d!="yes"):
+        print("thank you")
+        break
+    
+    
+ 
